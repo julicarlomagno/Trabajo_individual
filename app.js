ab684b877afc0d8df8ec609e73eadce6674e2299
julicarlomagno <julicarlomagno@gmail.com>
2023-05-06 16:41:55 -0300

    segundo video

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const publicPath = path.join(__dirname, './public')
-app.use(express.static(publicPath))
+app.use(express.static(public))
 app.listen(3000, ()=>{
     console.log('Servidor corriendo');
 })
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,'/views/home,html'))
+    res.sendFile(path.join(__dirname,'/views/home.html'))
 })
