635d943dafd7c9fcd3ac7f9db852b3366598951b
julicarlomagno <julicarlomagno@gmail.com>
2023-05-06 16:47:58 -0300

    foto bien

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use(express.static(public))
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath))
 app.listen(3000, ()=>{
     console.log('Servidor corriendo');
 })
